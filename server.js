a714320441530e1a5459ba432a9d57053b5c922b
Pooja Balsaraf <poojabalsaraf10@gmail.com>
2020-11-03 22:34:32 +0530

    deploying to heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/angularerram'));
+app.use(express.static(__dirname + '/dist/Angularerram'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/angularerram/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Angularerram/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
